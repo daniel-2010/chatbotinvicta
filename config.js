8445118f1eb36339f4f8c6a860fea3a1e5e069b6
Daniel <daniel-2.010@hotmail.com>
2018-07-19 14:47:54 -0300

    Initial Configuration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     FB_PAGE_TOKEN: 'EAAEEEQjZCi8cBAPlwSEiaiXd29TBTPaApOEVidYjky49AyJxaQF95GrvkMZBxZBMANhZC7uAPwKDTKanYGZC4cPnftlbaIXSMwPPi8rbIZBt9EbSjdcIMZCK9yLj6OpfdB6tjx1JRZBxekp4ZAO0ZCMTrcPbAQT6B4FVq6UZArDCtz7YG08BDch50cj',
     FB_VERIFY_TOKEN: 'this_is_my_large_password_for_the_bot',
-    API_AI_CLIENT_ACCESS_TOKEN: 'dc7bf36cff65409e94ee7c6c74cd2c6d',
+    API_AI_CLIENT_ACCESS_TOKEN: 'c06f0be17a2149cfb3648b0e17ad22e3',
     FB_APP_SECRET: '796e6e33b9f1ae9ffe2d4a339e30c7de',
     SERVER_URL: "https://pizzbotchat.herokuapp.com/",
 };
