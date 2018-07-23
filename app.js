e1c1d73e27c2db827f5f9ecd7134be532a2528c9
Daniel <daniel-2.010@hotmail.com>
2018-07-23 17:17:07 -0300

    Buttons

--- app.js
+++ app.js
@@ -350,6 +350,10 @@ function handleApiAiResponse(sender, response) {
 
 	sendTypingOff(sender);
 
+	if (isDefined(action)) {
+		handleApiAiAction(sender, action, responseText, contexts, parameters);
+	} 
+
 	if (isDefined(messages) && (messages.length == 1 && messages[0].type != 0 || messages.length > 1)) {
 		let timeoutInterval = 1100;
 		let previousType ;
