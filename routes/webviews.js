605d7b05dc11c27dc981aa173141dd749e0c0ee7
Daniel <daniel-2.010@hotmail.com>
2018-07-25 16:25:07 -0300

    Save users mongodb

--- routes/webviews.js
+++ routes/webviews.js
@@ -3,8 +3,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.send('Birds home page');
-  //res.render('newsletter-settings');
+  //res.send('Birds home page');
+  res.render('newsletter-settings');
 });
 
 module.exports = router;
