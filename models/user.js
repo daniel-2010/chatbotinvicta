a4bf619b1c3d0ec017bfbf6660630b2ec10360ad
Daniel <daniel-2.010@hotmail.com>
2018-07-25 10:25:10 -0300

    Save users mongodb

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  id: { type: String, required: true },
+  id: { type: String },
   fb_id: { type: String, required: true },
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
