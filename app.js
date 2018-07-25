c7190a6e7b8723fe56979b21e7af1a569f27701a
Daniel <daniel-2.010@hotmail.com>
2018-07-25 10:04:34 -0300

    Save users mongodb

--- app.js
+++ app.js
@@ -804,7 +804,7 @@ function greetUserText(userId) {
 					timezone: user.timezone
 				  });
 
-				  person.find({"fb_id": userId}).exec().then(
+				  person.find({"fb_id": userId}).then(createdUser =>
 					function(err,doc) {
 						if(err)
 						 console.log("Erro on findOne: "+err);
