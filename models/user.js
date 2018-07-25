1a5a5455c180e4b16cdfdf7e4fd72f5eefa0cd0e
Daniel <daniel-2.010@hotmail.com>
2018-07-25 09:33:59 -0300

    Save users mongodb

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const postSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
   fb_id: { type: String, required: true },
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
@@ -8,7 +8,6 @@ const postSchema = mongoose.Schema({
   locale: { type: String, required: true },
   gender: { type: String, required: true },
   timezone: { type: String, required: true }
-
 });
 
-module.exports = mongoose.model("user", postSchema);
+module.exports = mongoose.model("user", userSchema);
