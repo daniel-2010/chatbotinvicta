da8c7ff4f66cda57a4d6395e87ac4bc1a9fddb49
Daniel <daniel-2.010@hotmail.com>
2018-07-26 14:03:23 -0300

    Webviews

--- models/user.js
+++ models/user.js
@@ -8,7 +8,10 @@ const userSchema = mongoose.Schema({
   profile_pic: { type: String},
   locale: { type: String},
   gender: { type: String },
-  timezone: { type: String}
+  timezone: { type: String},
+  newsletter: { type: String},
+  topics: { type: String},
+  deals: { type: String}
 });
 
 module.exports = mongoose.model("user", userSchema);
--- routes/webviews.js
+++ routes/webviews.js
@@ -1,6 +1,12 @@
 
 var express = require('express');
+
+const mongoose = require("mongoose");
+const UserMongoose = require("../models/user");
+
 var router = express.Router();
+
+
 const fbservice = require('../fb-service/fb-service');
 router.get('/', function(req, res) {
   //res.send('Birds home page');
@@ -12,8 +18,22 @@ router.get('/save',function(req,res){
   let topics = body.topics.join(',');
   let response = `Newsletter: ${body.newsletter}, topics: ${topics} and deals ${body.deals}`;
   
+  UserMongoose.update({ fb_id:body.psid},
+    {$set: { newsletter:body.newsletter,topics:topics,deals:body.deals} },
+    function(err, count) {
+           if (err){
+            console.log('===> Erro ao atualizar Newsletter: '+err);
+           }else{
+            console.log('===> Newsletter  atualizado com sucesso! Conut:' + count);
+           }
+    });
+  
+  
   fbservice.sendTextMessage(body.psid,response);
-})
+});
+router.get('/settings',function(req,res){
+  
+});
 
 module.exports = router;
 
