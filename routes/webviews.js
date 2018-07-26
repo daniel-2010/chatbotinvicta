2039d3b78ac4141fd2463360a0df98e8af2dc7cf
Daniel <daniel-2.010@hotmail.com>
2018-07-26 09:25:51 -0300

    Webviews

--- routes/webviews.js
+++ routes/webviews.js
@@ -1,11 +1,19 @@
 
 var express = require('express');
 var router = express.Router();
-
+const fbservice = require('..fb-service/fb-service');
 router.get('/', function(req, res) {
   //res.send('Birds home page');
   res.render('../views/newsletter-settings');
 });
+
+router.get('/save',function(req,res){
+  let body = req.query;
+  let topics = body.topics.join(',');
+  let response = 'Newsletter: ${body.newsletter}, topics: ${topics} and deals ${body.deals}';
+  
+  fbservice.sendTextMessage(body.psid,response);
+})
 
 module.exports = router;
 
