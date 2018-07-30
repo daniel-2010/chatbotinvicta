d63685baf711d324db434e3995e382b433387f74
Daniel <daniel-2.010@hotmail.com>
2018-07-30 08:57:20 -0300

    First application

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     FB_PAGE_TOKEN: 'EAAalyJPcCX8BAIlIfaQUgboBQviiEbbufDyZAQpuierCK1sfZCcq6FGKPD0PLIk3Y7LqnuDUjqoUDxZBG1zdMyD70HLt4AcgiMgPgnN3okbG6O9h2Fa6dEmTAGY2umw8ftPeNzBawfZBKzY2blj7KIRuiKEzmcP7aeaLDQto9wZDZD',
     FB_VERIFY_TOKEN: 'this_is_my_large_password_for_the_bot',
     API_AI_CLIENT_ACCESS_TOKEN: 'f216f61b62ae4460b2cf0c0d6e9969f5',
-    FB_APP_SECRET: '796e6e33b9f1ae9ffe2d4a339e30c7de',
+    FB_APP_SECRET: 'f0ff5deabec21995add45fc1652c6c2e',
     SERVER_URL: "https://pizzariainvicta.herokuapp.com/",
     SENGRID_API_KEY: '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',
     EMAIL_FROM: 'daniel-2.010@hotmail.com',
