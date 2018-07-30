04c745dfb94e2071036883502a1578d60f16e048
Daniel <daniel-2.010@hotmail.com>
2018-07-30 08:36:38 -0300

    First application

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     API_AI_CLIENT_ACCESS_TOKEN: 'f216f61b62ae4460b2cf0c0d6e9969f5',
     FB_APP_SECRET: '796e6e33b9f1ae9ffe2d4a339e30c7de',
     SERVER_URL: "https://pizzariainvicta.herokuapp.com/",
-    SENGRID_API_KEY: '',
+    SENGRID_API_KEY: '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',
     EMAIL_FROM: 'daniel-2.010@hotmail.com',
     EMAIL_TO: 'daniel-2.010@hotmail.com',
     MONGODB_USER: 'root',
