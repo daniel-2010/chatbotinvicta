6acadf415c78ee32a65c81ac4e9ad88bf787d9bd
Daniel <daniel-2.010@hotmail.com>
2018-07-30 10:22:13 -0300

    Second application

--- app.js
+++ app.js
@@ -830,7 +830,7 @@ function greetUserText(userId) {
 				setTimeout(function(){
 					let buttons = [
 						{
-							title:"Ver cardápio e/ou fazer um pedido!",
+							title:"Cardápio/Pedido!",
 							type:"web_url",
 							url:"https://pizzariainvicta.herokuapp.com/webview",
 							webview_height_ratio: "tall",
@@ -838,7 +838,7 @@ function greetUserText(userId) {
 						},
 						{
 							type: 'postback',
-							title: "Fazer outra pergunta!",
+							title: "Outra Pergunta!",
 							payload: "CHAT"
 						}
 					]
