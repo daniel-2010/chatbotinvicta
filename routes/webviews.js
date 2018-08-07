24eb1da9f9982ec0811be91698179d7e1c35f953
Daniel <daniel-2.010@hotmail.com>
2018-08-07 10:40:08 -0300

    Save Sales

--- routes/webviews.js
+++ routes/webviews.js
@@ -73,13 +73,7 @@ router.get('/save', function (req, res) {
           let borderBanco = {};
 
           if (body['product_' + doc._id + '_borda'].length > 0) {
-            bordersModel.findOne({ "_id": body['product_' + doc._id + '_borda'] }, function (err, doc1) {
-              if (doc1) {
-                borderBanco = doc1;
-              }
-              console.log("####>>> 1 Nome borda: "+borderBanco.nome_border);
-              //console.log("####>>> 1 Nome borda Doc: "+doc1.nome_border);
-            })
+            borderBanco = bordersModel.findOne({ "_id": body['product_' + doc._id + '_borda'] }, function (err, doc1) {});
             console.log("####>>> 2 Nome borda: "+borderBanco.nome_border);
           }
 
