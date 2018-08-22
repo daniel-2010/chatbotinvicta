a326dd3cd0a8296f2755d9f47ee5a6c68d044b74
Daniel <daniel-2.010@hotmail.com>
2018-08-22 16:18:13 -0300

    Last version

--- app.js
+++ app.js
@@ -465,7 +465,7 @@ function handleApiAiResponse(sender, response) {
 	} else if (responseText == '' && !isDefined(action)) {
 		//api ai could not evaluate input.
 		console.log('Unknown query' + response.result.resolvedQuery);
-		sendTextMessage(sender, "I'm not sure what you want. Can you be more specific?");
+		sendTextMessage(sender, "Não consegui entender o que você disse. Em que posso melhor te ajudar?");
 	} else if (isDefined(action)) {
 		//handleApiAiAction(sender, action, responseText, contexts, parameters);
 	} else if (isDefined(responseData) && isDefined(responseData.facebook)) {
