b4d9d6a1a55245176d61218d89e2dbe2bbb2a0fe
Daniel <daniel-2.010@hotmail.com>
2018-09-03 10:31:12 -0300

    Update token access

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAAalyJPcCX8BAIlIfaQUgboBQviiEbbufDyZAQpuierCK1sfZCcq6FGKPD0PLIk3Y7LqnuDUjqoUDxZBG1zdMyD70HLt4AcgiMgPgnN3okbG6O9h2Fa6dEmTAGY2umw8ftPeNzBawfZBKzY2blj7KIRuiKEzmcP7aeaLDQto9wZDZD',
+    FB_PAGE_TOKEN: 'EAAalyJPcCX8BAFqMeJjRE8hSijYmZBPBZCZCPHs5EDri9afgLPqhii4GhG4xgHAY7XvwPi3ZBN83hIRUpSLG24icbvLJ28dXe9llIy8TgjpJhI4Gm9Bz1dq3OasZBmZAyNdbkeuQmLFvSGWdwrU4ffj7UUMvdZBX2ENma5MIF3xgwZDZD',
+    
     FB_VERIFY_TOKEN: 'this_is_my_large_password_for_the_bot',
     API_AI_CLIENT_ACCESS_TOKEN: 'f216f61b62ae4460b2cf0c0d6e9969f5',
     FB_APP_SECRET: 'f0ff5deabec21995add45fc1652c6c2e',
